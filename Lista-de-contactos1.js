0e11e4bff5e04c1007efe04aa1b5e748defb0524
Alejandra Lopez <mlopezalejandra13@gmail.com>
2023-08-22 22:04:27 -0500

    ajustando funciones a objetos

--- Lista-de-contactos1.js
+++ Lista-de-contactos1.js
@@ -1,37 +1,54 @@
 
 // Crea una lista de contactos con datos predefinidos, cada contacto debe contener el nombre y apellido como una sola cadena de caracteres
-
-let listaContactos = ["AlejandraLopez", "MarcelaMuñoz", "RobertoLopez"];
+let listaContactos = [{
+  id :1,
+  nombre: "Alejandra",
+  apellido: "Lopez",
+  telefono: "3168067141",
+  ubicaciones: {
+    ciudad: "Pasto",
+    direccion:"mz c casa 10",}
+}
+]
 
 // // Crea una función para añadir un nuevo contacto a una lista
 function agregarContacto (nuevoContacto){
   listaContactos.push(nuevoContacto)
-  return listaContactos
 }
 // agregarContacto("LuisBacca");
 // agregarContacto("SofiaMuñoz");
 
 // console.log(listaContactos);
 
-// // Crea una función para borrar un contacto existente de la lista
+// // // Crea una función para borrar un contacto existente de la lista
 function borrarContacto(contactoABorrar){
-  let nuevaLista = listaContactos.filter((contacto) => contacto !==contactoABorrar)
+  let nuevaLista = listaContactos.filter(
+  (contacto) => contacto.id !==contactoABorrar)
   listaContactos = nuevaLista
 }
 
-// borrarContacto ("AlejandraLopez")
+// // borrarContacto ("AlejandraLopez")
 
-// console.log(listaContactos);
+// // console.log(listaContactos);
 
-// // Crea una función para imprimir en consola los contactos presentes en la lista
+// // // Crea una función para imprimir en consola los contactos presentes en la lista
 
 function imprimirContactos (){
   console.log(listaContactos);
 }
 
-// agregarContacto("LuisBacca");
-agregarContacto("LuisBacca");
-agregarContacto("SofiaMuñoz");
-borrarContacto("SofiaMuñoz");
+let nuevoContacto= {
+  id :2,
+  nombre: "Maria",
+  apellido: "Muñoz",
+  telefono: "31226777787",
+  ubicaciones: {
+    ciudad: "Ipiales",
+    direccion:"mz c casa 7",}
+}
+
+
+// agregarContacto(nuevoContacto);
+borrarContacto(2);
 imprimirContactos()
-// Publica tu código en Github, en tu repositorio contact-list crea un branch llamado project-1 y compártelo con nosotros. 👍🏼⬇️
+// // Publica tu código en Github, en tu repositorio contact-list crea un branch llamado project-1 y compártelo con nosotros. 👍🏼⬇️
